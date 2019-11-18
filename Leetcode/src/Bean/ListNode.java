9078d7082fc21cc257a1b5c2340c0daa0f561991
fengxiaotao <1577489928@qq.com>
2019-11-18 18:07:36 +0800

    ✨✨

---
+++ Leetcode/src/Bean/ListNode.java
@@ -0,0 +1,9 @@
+package Bean;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+    }
+}
---
+++ LinkedList/src/Lnode.java
@@ -0,0 +1,4 @@
+public class Lnode {
+    public char data;
+    public Lnode next;
+}
