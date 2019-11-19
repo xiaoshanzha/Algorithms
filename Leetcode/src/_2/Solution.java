c3cee9982aae8cbaf63452d1059a6ebb87f0ee3a
fengxiaotao <1577489928@qq.com>
2019-11-19 22:01:54 +0800

    ✨✨

---
+++ Leetcode/src/_2/Solution.java
@@ -0,0 +1,37 @@
+package _2;
+
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode head = new ListNode(0);
+        ListNode p , q ,cur;
+        cur = head;
+        p = l1;
+        q = l2;
+        while(p!=null||q!=null){
+            int x = p!=null?p.val:0;
+            int y = q!=null?q.val:0;
+            int sum = x+y+carry;
+            carry = (sum)/10;
+            cur.next= new ListNode(sum%10);
+            cur = cur.next;
+            if(p!=null){
+                p = p.next;
+            }
+            if(q!=null){
+                q = q.next;
+            }
+        }
+        if(carry>0){
+            cur.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode l1 = new ListNode(new int[]{1});
+        ListNode l2 = new ListNode(new int[]{9,9});
+        System.out.println( solution.addTwoNumbers(l1,l2));
+    }
+}
--- Leetcode/src/_83/Solution.java
+++ Leetcode/src/_83/Solution.java
@@ -1,6 +1,9 @@
 package _83;
 
 public class Solution {
+    /*
+    * 初
+    * */
     public ListNode deleteDuplicates(ListNode head) {
         ListNode h = new ListNode(0);
         ListNode p = h;
@@ -8,7 +11,6 @@ public class Solution {
         p = p.next;
         ListNode cur = head;
         while(cur!=null){
-            System.out.println("p:"+p.val+"   cur:"+cur.val);
             if(p.val==cur.val){
                 cur = cur.next;
                 // 没有以下语句会出现 末结点重复删除不掉
@@ -22,11 +24,28 @@ public class Solution {
         return h.next;
     }
 
+    /*
+    * 改：
+    * 将结点的值与它之后的结点进行比较来确定它是否为重复结点。
+    * 如果它是重复的，更改当前结点的 next 指针，
+    * 以便它跳过下一个结点并直接指向下一个结点之后的结点。
+    * */
+    public ListNode deleteDuplicates1(ListNode head) {
+        // cur一直指向相同数字第一次出现的结点
+        ListNode cur = head;
+        while(cur!=null&&cur.next!=null){
+            if(cur.val==cur.next.val){
+                cur.next = cur.next.next;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         ListNode l1 = new ListNode(new int[]{1,1,2,3,3});
-        System.out.println(solution.deleteDuplicates(l1));
+        System.out.println(solution.deleteDuplicates1(l1));
     }
-
-
 }
