b568a957a638415449b0716983cd6800c70ac02d
fengxiaotao <1577489928@qq.com>
2019-11-20 12:31:01 +0800

    ✨✨

--- Pro_con/src/cn/edu/nuc/Design_Patterns/behavior/flyBehavior.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.nuc.Design_Patterns.behavior;
-
-public interface flyBehavior {
-    /*
-    * 飞行
-    * */
-    void fly();
-}
--- Pro_con/src/cn/edu/nuc/Design_Patterns/behavior/singBehavior.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.nuc.Design_Patterns.behavior;
-
-public interface singBehavior {
-    /*
-    * 唱歌
-    * */
-    void sing();
-}
