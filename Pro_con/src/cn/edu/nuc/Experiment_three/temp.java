4af7c6be5712cc66f6f59ff72c2da4dc95583a7a
fengxiaotao <1577489928@qq.com>
2019-11-26 20:45:20 +0800

    ✨✨

--- Pro_con/src/cn/edu/nuc/Experiment_three/temp.java
+++ Pro_con/src/cn/edu/nuc/Experiment_three/temp.java
@@ -1,11 +1,29 @@
 package cn.edu.nuc.Experiment_three;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class temp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileReader fr =  new FileReader("F:\\GitHub\\Algorithms\\Pro_con\\src\\cn\\edu\\nuc\\Experiment_three\\temp.txt");
+        FileWriter fw =  new FileWriter("F:\\GitHub\\Algorithms\\Pro_con\\src\\cn\\edu\\nuc\\Experiment_three\\temp.txt");
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        System.out.println(s);
+        String temp = scanner.nextLine();
+        char[] chars = temp.toCharArray();
+        for(int i = 0;i<chars.length;i++) {
+            char c = chars[i];
+            fw.write(c);
+            fw.flush();
+            System.out.println("正在录入字符："+c);
+        }
+        char[] read = new char[1024];
+        String rea = "";
+        int len ;
+        while((len = fr.read(read))!=-1){
+            rea = rea + String.valueOf(read);
+        }
+        System.out.println("录入完成，录入信息为：" + rea);
+        fw.close();
+        fr.close();
     }
 }
