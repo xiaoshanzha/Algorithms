3b5c53c338675d9288ded97a690e7b79fa56d804
fengxiaotao <1577489928@qq.com>
2020-04-27 23:13:39 +0800

    Binary

---
+++ Code/src/LeetCode/ordinary/Code33_search.java
@@ -0,0 +1,36 @@
+package LeetCode.ordinary;
+
+public class Code33_search {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            int mid = start + (end -start)/2;
+            if (nums[mid] == target){
+                return mid;
+            }
+            //后半部分有序
+            if(nums[mid] < nums[end]){
+                if(nums[mid] < target && target <= nums[end]){
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+
+            } else {
+                if(nums[mid] > target && target >= nums[start]){
+                    end = mid - 1;
+
+                } else {
+                    start = mid + 1;
+
+                }
+            }
+        }
+        return -1;
+    }
+}
