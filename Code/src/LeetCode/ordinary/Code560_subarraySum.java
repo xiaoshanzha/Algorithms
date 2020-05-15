26577c13b794bbdb24795670ce54efbcd1e948c4
fengxiaotao <1577489928@qq.com>
2020-05-15 23:06:29 +0800

    KS

---
+++ Code/src/LeetCode/ordinary/Code560_subarraySum.java
@@ -0,0 +1,23 @@
+package LeetCode.ordinary;
+
+public class Code560_subarraySum {
+    public int subarraySum(int[] nums, int k) {
+        int len = nums.length;
+        // 计算前缀和数组
+        int[] preSum = new int[len + 1];
+        preSum[0] = 0;
+        for (int i = 0; i < len; i++) {
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+        int count = 0;
+        for (int left = 0; left < len; left++) {
+            for (int right = left; right < len; right++) {
+                // 区间和 [left..right]，注意下标偏移
+                if (preSum[right + 1] - preSum[left] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
